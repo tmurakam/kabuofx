fa6151a02126d28cedd4e2e6188fc30fc4a157b8
Takuya Murakami <tmurakam@tmurakam.org>
2013-07-20 12:23:13 +0900

    updated

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,7 @@ class StocksController < ApplicationController
   # GET /stocks
   # GET /stocks.json
   def index
-    @stocks = Stock.limit(30).all
+    @stocks = Stock.all
   end
 
   # GET /stocks/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Kabuofx::Application.routes.draw do
-  resources :stocks
+  resources :stocks, :only => [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

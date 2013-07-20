03e443d11543c4afebcf35936121b9dfce8a9470
Takuya Murakami <tmurakam@tmurakam.org>
2013-07-20 11:11:57 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kabuofx::Application.config.secret_key_base = '4c6aec1c997f5603d9e57ccf7daa0907f30192208ec72a2d43446273fa23f342e7eafef26faad547fb36405458ca9ff1f259053c8bcf47ffcfab10b50f36a1e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kabuofx::Application.config.session_store :cookie_store, key: '_kabuofx_session'

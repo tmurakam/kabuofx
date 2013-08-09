6b219bfa5efc7680c36757032d5a56ae0718ae66
Takuya Murakami <tmurakam@tmurakam.org>
2013-08-09 21:03:58 +0900

    add rest api: get stock name

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,18 @@
+# -*- coding: utf-8 -*-
 class StocksController < ApplicationController
   # GET /stocks
   # GET /stocks.json
   def index
     @stocks = Stock.all
+  end
+
+  # 証券名取得
+  def name
+    @stock = Stock.where(:code => params[:code]).first
+    json = {}
+    if @stock
+      json = {:code => @stock.code, :name => @stock.name}
+    end
+    render :json => json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Kabuofx::Application.routes.draw do
   get "downloads/codes" => "downloads#codes"
 
   resources :stocks, :only => [:index]
+  get "stocks/name/:code" => "stocks#name"
 
   root 'downloads#index'
 end

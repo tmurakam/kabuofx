432bfd2e3700f4ad9b5ff21196c447be55fb9349
Takuya Murakami <tmurakam@tmurakam.org>
2013-11-28 12:15:36 +0900

    fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :scm, :git
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{log public/ofx}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

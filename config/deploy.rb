2206aa9c87e2c780433683bc69f6049ef08e00b3
Takuya Murakami <tmurakam@tmurakam.org>
2013-11-28 12:05:14 +0900

    fix capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'kabuofx'
+set :repo_url, 'https://github.com/tmurakam/kabuofx.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :deploy_to, '/home/rails/kabuofx'
+set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
@@ -22,7 +22,8 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :mkdir, '-p', release_path.join('tmp')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 

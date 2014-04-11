148749612441a4b5968db92cb4d9132785423f3b
Takuya Murakami <tmurakam@tmurakam.org>
2014-04-11 23:07:47 +0900

    Fix kabu CSV load logic

--- app/models/download/download_stocks.rb
+++ app/models/download/download_stocks.rb
@@ -6,10 +6,17 @@ require 'open-uri'
 class DownloadStocks
   # 株価情報をダウンロードする
   def download
-    url = "http://k-db.com/site/download.aspx?p=all&download=csv"
+    #旧URL
+    #url = "http://k-db.com/site/download.aspx?p=all&download=csv"
+
+    #now = Time.now
+    #today = now.strftime("%Y-%m-%d")
+    today = Date.today
+    url = "http://k-db.com/stocks/#{today.to_s}?download=csv"
+
     open(url, 'r:Shift_JIS') do |data|
       csv = data.read
-      import_csv(csv)
+      import_csv(csv, today)
     end
 
     OfxGen.new.generate_all
@@ -19,23 +26,24 @@ class DownloadStocks
   private
 
   # 株価 CSV ファイルをパースし、データを保存
-  def import_csv(csv)
+  def import_csv(csv, date)
     ActiveRecord::Base::transaction() do
       lineno = 0
-      date = nil
+      #date = nil
       CSV.parse(csv) do |cols|
         lineno += 1
 
         # 1行目
         if (lineno == 1)
           # 日付を取得する
-          date = Date.parse(cols[0])
+          #date = Date.parse(cols[0])
           next
         end
 
         if (lineno == 2)
           # チェック
           if (cols.length != 10 || cols[0] != "コード" ||
+          cols[2] != "銘柄名" ||
               cols[7] != "終値")
             throw "Invalid CSV file format (2nd line"
           end
@@ -44,7 +52,7 @@ class DownloadStocks
 
         # 通常行
         code = cols[0]
-        name = cols[1]
+        name = cols[2]
         price = cols[7].to_f
         
         # コードチェック
@@ -56,6 +64,9 @@ class DownloadStocks
 
         # 価格チェック
         next if price == 0
+
+        # debug
+        #pp "----", code, name, price, date
 
         # 既存エントリを探す
         stock = Stock.where(code: code).first
